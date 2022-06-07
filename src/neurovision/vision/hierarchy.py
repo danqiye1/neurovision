003287b70abb54b7869d4a7f43791d9dedb45b76
danqiye1 <5430399+danqiye1@users.noreply.github.com>
2022-06-07 18:00:03 +0800

    WIP: visual hierarchy training

--- src/neurovision/vision/hierarchy.py
+++ src/neurovision/vision/hierarchy.py
@@ -9,7 +9,6 @@ import nengo_dl
 import numpy as np
 import tensorflow as tf
 
-
 def build_visual_hierarchy(shape_in, size_out):
     """
     Function for constructing a nengo network.
@@ -32,7 +31,7 @@ def build_visual_hierarchy(shape_in, size_out):
         conv2 = nengo_dl.Layer(tf.keras.layers.Conv2D(filters=64, strides=2, kernel_size=3))
         conv3 = nengo_dl.Layer(tf.keras.layers.Conv2D(filters=128, strides=2, kernel_size=3))
         ensemble_layer = nengo_dl.Layer(nengo.LIF(amplitude=0.01))
-        out_layer = nengo_dl.Layer(tf.keras.layers.Dense(units=10))
+        out_layer = nengo_dl.Layer(tf.keras.layers.Dense(units=size_out))
 
         # Forward pass
         x = conv1(input, shape_in=shape_in)
@@ -44,3 +43,33 @@ def build_visual_hierarchy(shape_in, size_out):
         out = out_layer(x)
 
     return net
+
+def train_network(sim, X_train, y_train, epochs):
+    sim.compile(
+        optimizer=tf.optimizers.RMSprop(0.001),
+        loss={"out_p": tf.losses.SparseCategoricalCrossentropy(from_logits=True)}
+    )
+
+    sim.fit(X_train, {"out_p": y_train}, epochs=epochs)
+    return sim
+    
+
+if __name__ == "__main__":
+    # Load and preprocess data
+    (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+    shape = X_train.shape
+    X_train = X_train.reshape((len(X_train), -1))
+    X_test = X_test.reshape((len(X_test), -1))
+    # Add time dimension
+    X_train = X_train[:, None, :]
+    y_train = y_train[:, None, None]
+    # Repeat input/target for n_steps
+    n_steps = 30
+    X_test = np.tile(X_test[:, None, :], (1, n_steps, 1))
+    y_test = np.tile(y_test[:, None, None], (1, n_steps, 1))
+
+    # Build network and simulator
+    net = build_visual_hierarchy((28,28,1), 10)
+    minibatch_size = 200
+    sim = nengo_dl.Simulator(net, minibatch_size=minibatch_size)
+    train_network(sim, X_train, y_train, 10)
