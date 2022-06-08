c1ab4c2e20dec194e8bcfe54fb3fd0a3fe7c1aed
danqiye1 <5430399+danqiye1@users.noreply.github.com>
2022-06-08 11:04:00 +0800

    WIP: Training method complete. Ready to test on GPU

--- src/neurovision/vision/hierarchy.py
+++ src/neurovision/vision/hierarchy.py
@@ -16,7 +16,7 @@ def build_visual_hierarchy(shape_in, size_out):
     :param shape_in: dimension of input (usually is number of image pixels)
     :param size_out: dimension of output (usually is number of classes)
     """
-    with nengo.Network(seed=0) as net:
+    with nengo.Network(seed=0, label="Visual Hierarchy") as net:
         # Default parameters for all neurons
         net.config[nengo.Ensemble].max_rates = nengo.dists.Choice([100])
         net.config[nengo.Ensemble].intercepts = nengo.dists.Choice([0])
@@ -26,7 +26,7 @@ def build_visual_hierarchy(shape_in, size_out):
         nengo_dl.configure_settings(stateful=False)
 
         # Build the neural network
-        input = nengo.Node(np.zeros(np.prod(shape_in)))
+        input = nengo.Node(np.zeros(np.prod(shape_in)), label="Input")
         conv1 = nengo_dl.Layer(tf.keras.layers.Conv2D(filters=32, kernel_size=3))
         conv2 = nengo_dl.Layer(tf.keras.layers.Conv2D(filters=64, strides=2, kernel_size=3))
         conv3 = nengo_dl.Layer(tf.keras.layers.Conv2D(filters=128, strides=2, kernel_size=3))
@@ -34,23 +34,47 @@ def build_visual_hierarchy(shape_in, size_out):
         out_layer = nengo_dl.Layer(tf.keras.layers.Dense(units=size_out))
 
         # Forward pass
-        x = conv1(input, shape_in=shape_in)
-        x = ensemble_layer(x)
-        x = conv2(x, shape_in=(26, 26, 32))
-        x = ensemble_layer(x)
-        x = conv3(x, shape_in=(12, 12, 64))
-        x = ensemble_layer(x)
-        out = out_layer(x)
+        x = conv1(input, shape_in=shape_in, label="conv1")
+        x = ensemble_layer(x, label="conv1")
+        x = conv2(x, shape_in=(26, 26, 32), label="conv2")
+        x = ensemble_layer(x, label="conv2")
+        x = conv3(x, shape_in=(12, 12, 64), label="conv3")
+        x = ensemble_layer(x, label="conv3")
+        out = out_layer(x, label="output")
 
     return net
 
-def train_network(sim, X_train, y_train, epochs):
+def train_network(net, X_train, y_train, epochs=10, batch_size=200):
+    """
+    Train the Nengo neural network leveraging on Tensorflow.
+
+    :param net: Nengo network to train.
+    :type net: nengo.Network
+    :param X_train: Training data of shape (N, D)
+    :type X_train: np.ndarray
+    :param y_train: Training labels (N, 1). Not one-hot-encoded.
+    :type y_train: np.ndarray
+
+    :param epochs: Number of training epochs.
+    :param batch_size: Minibatch size.
+    """
+    # Handle on output
+    output = net.nodes[-1]
+    assert output.label == "output", \
+        f"Last node in {net.label} is f{output.label} instead of 'output'. \
+            Check if last node is output node."
+
+    # Add a probe
+    with net:
+        out_p = nengo.Probe(output, label="out_p")
+
+    sim = nengo_dl.Simulator(net, minibatch_size=batch_size)
     sim.compile(
         optimizer=tf.optimizers.RMSprop(0.001),
-        loss={"out_p": tf.losses.SparseCategoricalCrossentropy(from_logits=True)}
+        loss={out_p: tf.losses.SparseCategoricalCrossentropy(from_logits=True)}
     )
 
-    sim.fit(X_train, {"out_p": y_train}, epochs=epochs)
+    sim.fit(X_train, {out_p: y_train}, epochs=epochs)
     return sim
     
 
@@ -68,8 +92,6 @@ if __name__ == "__main__":
     X_test = np.tile(X_test[:, None, :], (1, n_steps, 1))
     y_test = np.tile(y_test[:, None, None], (1, n_steps, 1))
 
-    # Build network and simulator
+    # Build network and train
     net = build_visual_hierarchy((28,28,1), 10)
-    minibatch_size = 200
-    sim = nengo_dl.Simulator(net, minibatch_size=minibatch_size)
-    train_network(sim, X_train, y_train, 10)
+    train_network(net, X_train, y_train)
