1244e9f1b597c8fccc6b1664efe471cdc1dea41b
danqiye1 <5430399+danqiye1@users.noreply.github.com>
2022-07-01 12:16:00 +0800

    Add one-layer-mnist classification with PES

--- experiments/autoencoder.py
+++ experiments/autoencoder.py
@@ -20,15 +20,20 @@ model = nengo.Network()
 rng = np.random.RandomState(9)
 
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
-X_train = X_train.reshape(len(X_train), -1)
+img_dim = X_train[0].reshape(-1).shape[0]
 hidden_dim = 392
+
+def preprocess(img):
+    X = img.reshape(-1)
+    X = X - np.mean(X)
+    return X / np.linalg.norm(X)
 
 with model:
     model.config[nengo.Ensemble].max_rates = nengo.dists.Choice([100])
     model.config[nengo.Ensemble].intercepts = nengo.dists.Choice([0])
     model.config[nengo.Connection].synapse = None
     
-    input = nengo.Node(lambda t: X_train[int(t), :])
+    input = nengo.Node(lambda t: preprocess(X_train[int(t) // 2]))
     
     # Encoders are used as transforms for dimension reduction
     encoders = Mask((28,28)).populate(
@@ -45,18 +50,18 @@ with model:
     
     in_ensemble = nengo.networks.EnsembleArray(
         n_neurons=30,
-        n_ensembles=X_train.shape[1],
+        n_ensembles=img_dim,
         ens_dimensions=1
     )
 
     hidden = nengo.Ensemble(
-        n_neurons=100,
+        n_neurons=5000,
         dimensions=hidden_dim
     )
 
     out_ensemble = nengo.networks.EnsembleArray(
         n_neurons=30,
-        n_ensembles=X_train.shape[1],
+        n_ensembles=img_dim,
         ens_dimensions=1
     )
     
@@ -64,7 +69,7 @@ with model:
     nengo.Connection(input, in_ensemble.input)
     conn1 = nengo.Connection(
         in_ensemble.output, hidden, 
-        transform=encoders,
+        transform=encoders
     )
     conn2 = nengo.Connection(
         hidden, out_ensemble.input, 
@@ -76,16 +81,13 @@ with model:
     # and error signal connection
     recon_error = nengo.Ensemble(
         n_neurons=784,
-        dimensions=X_train.shape[1]
+        dimensions=784
     )
     nengo.Connection(out_ensemble.output, recon_error, transform=-1)
-    nengo.Connection(input, recon_error)
+    nengo.Connection(in_ensemble.output, recon_error)
     nengo.Connection(
         recon_error, conn2.learning_rule
     )
-
-    error_disp_node = nengo.Node(size_in=1)
-    nengo.Connection(recon_error, error_disp_node, function=lambda x: np.sum(np.square(x)))
 
     # Input image display (for nengo_gui)
     image_shape = (1, 28, 28)
@@ -93,7 +95,7 @@ with model:
     display_node = nengo.Node(display_func, size_in=input.size_out)
     nengo.Connection(input, display_node, synapse=None)
 
-    output = nengo.Node(display_func, size_in=X_train.shape[1])
+    output = nengo.Node(display_func, size_in=img_dim)
     nengo.Connection(out_ensemble.output, output, synapse=0.1)
     
     
