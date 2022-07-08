417f36bd99f89118288ff0c9e966d73548495cf2
danqiye1 <5430399+danqiye1@users.noreply.github.com>
2022-07-08 15:39:09 +0800

    WIP: Update to autoencoder - Simulator in NengoGUI crashes - Error connected to conn1 instead of conn2 - Gabor encoders used to manage dimensions - Decoding function to be learned with NEF

--- experiments/autoencoder.py
+++ experiments/autoencoder.py
@@ -29,34 +29,36 @@ def preprocess(img):
     return X / np.linalg.norm(X)
 
 with model:
-    model.config[nengo.Ensemble].max_rates = nengo.dists.Choice([100])
-    model.config[nengo.Ensemble].intercepts = nengo.dists.Choice([0])
-    model.config[nengo.Connection].synapse = None
-    
-    input = nengo.Node(lambda t: preprocess(X_train[int(t) // 2]))
-    
-    # Encoders are used as transforms for dimension reduction
+    # Gabor encoders are used as transforms from input dimension to hidden dimension
     encoders = Mask((28,28)).populate(
         Gabor().generate(hidden_dim, (11,11), rng=rng),
         rng=rng, 
         flatten=True
     )
-    # Decoder transforms/weights for reconstruction
-    decoders = Mask((28, 28)).populate(
-        Gabor().generate(hidden_dim, (11,11), rng=rng),
-        rng=rng,
-        flatten=True
-    )
-    
-    in_ensemble = nengo.networks.EnsembleArray(
-        n_neurons=30,
-        n_ensembles=img_dim,
-        ens_dimensions=1
+
+    # MNIST input feed
+    input = nengo.Node(lambda t: preprocess(X_train[int(t)]))
+
+    # Neural representation of MNIST image
+    in_ensemble = nengo.Ensemble(
+        n_neurons=6000,
+        dimensions=img_dim,
     )
 
+    nengo.Connection(input, in_ensemble)
+
+    # Hidden layer
     hidden = nengo.Ensemble(
         n_neurons=5000,
         dimensions=hidden_dim
+    )
+
+    # Feedforward encoding connection
+    # We want to learn f(x) using PES learning rule.
+    conn1 = nengo.Connection(
+        in_ensemble, hidden, 
+        transform=encoders,
+        learning_rule_type=nengo.PES()
     )
 
     out_ensemble = nengo.networks.EnsembleArray(
@@ -65,35 +67,32 @@ with model:
         ens_dimensions=1
     )
     
-    # Feedforward connections
-    nengo.Connection(input, in_ensemble.input)
-    conn1 = nengo.Connection(
-        in_ensemble.output, hidden, 
-        transform=encoders
-    )
+    # Feedforward decoding connection
+    # These are solved using NEF by evaluating on the MNIST points
+    eval_points = np.dot(X_train.reshape(len(X_train), -1), encoders.T)
     conn2 = nengo.Connection(
-        hidden, out_ensemble.input, 
-        transform=decoders.T,
-        learning_rule_type=nengo.PES()
+        hidden, out_ensemble.input,
+        eval_points=eval_points,
+        function=X_train.reshape(len(X_train), -1)
     )
-    
+
     # Error signal calculation
     # and error signal connection
     recon_error = nengo.Ensemble(
         n_neurons=784,
-        dimensions=784
+        dimensions=hidden_dim
     )
-    nengo.Connection(out_ensemble.output, recon_error, transform=-1)
-    nengo.Connection(in_ensemble.output, recon_error)
+    nengo.Connection(out_ensemble.output, recon_error, transform=-1*encoders)
+    nengo.Connection(input, recon_error, transform=encoders)
     nengo.Connection(
-        recon_error, conn2.learning_rule
+        recon_error, conn1.learning_rule
     )
 
     # Input image display (for nengo_gui)
     image_shape = (1, 28, 28)
     display_func = image_display_function(image_shape, offset=1, scale=128)
-    display_node = nengo.Node(display_func, size_in=input.size_out)
-    nengo.Connection(input, display_node, synapse=None)
+    input_disp = nengo.Node(display_func, size_in=input.size_out)
+    nengo.Connection(input, input_disp, synapse=None)
 
     output = nengo.Node(display_func, size_in=img_dim)
     nengo.Connection(out_ensemble.output, output, synapse=0.1)
